d89f1fb748422fb7bfb3311e553a7d51b3c1ceff
Laurence Elsdon <git@elsdon.me>
2023-04-09 07:35:30 +0100

    feat: Initial commit

---
+++ StudioLE.Core/src/Results/IResult.cs
@@ -0,0 +1,16 @@
+﻿namespace StudioLE.Core.Results;
+
+public interface IResult
+{
+    public string[] Warnings { get; set; }
+
+    public string[] Errors { get; }
+}
+
+// ReSharper disable once UnusedTypeParameter
+public interface IResult<out T>
+{
+    public string[] Warnings { get; set; }
+
+    public string[] Errors { get; }
+}
