3ba678786e0e67df86f0ebf868a300b1b606d7fb
Laurence Elsdon <git@elsdon.me>
2023-04-28 18:40:59 +0100

    feat: Added ResultHelpers.GetValueOrThrow()

--- StudioLE.Core/src/Results/ResultHelpers.cs
+++ StudioLE.Core/src/Results/ResultHelpers.cs
@@ -1,4 +1,6 @@
-﻿namespace StudioLE.Core.Results;
+﻿using StudioLE.Core.System;
+
+namespace StudioLE.Core.Results;
 
 public static class ResultHelpers
 {
@@ -7,5 +9,37 @@ public static class ResultHelpers
         return result is Success<T> success
             ? success.Value
             : null;
+    }
+
+    /// <summary>
+    /// Validate the <paramref name="result"/>.
+    /// Throw an exception if it is not a success.
+    /// </summary>
+    public static T GetValueOrThrow<T>(this IResult<T> result, string? contextMessage = null)
+    {
+        if (result is Success<T> success)
+            return success;
+        string message = contextMessage is null
+            ? string.Empty
+            : contextMessage + Environment.NewLine;
+        if (result.Errors.Any())
+            message += result.Errors.Join() + Environment.NewLine;
+        throw new(message);
+    }
+
+    /// <summary>
+    /// Validate the <paramref name="result"/>.
+    /// Throw an exception if it is not a success.
+    /// </summary>
+    public static void ThrowOnFailure(this IResult result, string? contextMessage = null)
+    {
+        if (result is Success)
+            return;
+        string message = contextMessage is null
+            ? string.Empty
+            : contextMessage + Environment.NewLine;
+        if (result.Errors.Any())
+            message += result.Errors.Join() + Environment.NewLine;
+        throw new(message);
     }
 }
