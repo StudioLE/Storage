00b0526805e629cf3675e31d4bc8cb352183ae57
Laurence Elsdon <git@elsdon.me>
2023-07-04 12:56:01 +0100

    feat(StudioLE.Core): Added ISerializer and IDeserializer

---
+++ StudioLE.Core/src/Serialization/IDeserializer.cs
@@ -0,0 +1,22 @@
+using System.IO;
+
+namespace StudioLE.Core.Serialization;
+
+/// <summary>
+/// A generic object deserializer.
+/// </summary>
+public interface IDeserializer
+{
+    /// <summary>
+    /// The file extension of the serialized format.
+    /// </summary>
+    public string FileExtension { get; }
+
+    /// <summary>
+    /// Deserialize the <paramref name="input"/> to <paramref name="type"/>.
+    /// </summary>
+    /// <param name="input">The serialized input.</param>
+    /// <param name="type">The type to deserialize to.</param>
+    /// <returns>The deserialized value.</returns>
+    public object? Deserialize(TextReader input, Type type);
+}
---
+++ StudioLE.Core/src/Serialization/ISerializer.cs
@@ -0,0 +1,21 @@
+using System.IO;
+
+namespace StudioLE.Core.Serialization;
+
+/// <summary>
+/// A generic object serializer.
+/// </summary>
+public interface ISerializer
+{
+    /// <summary>
+    /// The file extension of the serialized format.
+    /// </summary>
+    public string FileExtension { get; }
+
+    /// <summary>
+    /// Serialize the <paramref name="obj"/> to the <paramref name="writer"/>.
+    /// </summary>
+    /// <param name="writer">The writer to use.</param>
+    /// <param name="obj">The object to serialize.</param>
+    public void Serialize(TextWriter writer, object? obj);
+}
