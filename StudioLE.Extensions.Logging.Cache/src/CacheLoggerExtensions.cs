a09f5a0b82541203b3eed877b787e7040b8297a7
Laurence Elsdon <git@elsdon.me>
2023-12-17 20:42:16 +0000

    fix(StudioLE.Extensions.Logging.Cache): Fix the service extension names.

--- StudioLE.Extensions.Logging.Cache/src/CacheLoggerExtensions.cs
+++ StudioLE.Extensions.Logging.Cache/src/CacheLoggerExtensions.cs
@@ -12,13 +12,19 @@ public static class CacheLoggerExtensions
     /// <summary>
     /// Add a <see cref="CacheLogger"/>.
     /// </summary>
-    public static ILoggingBuilder AddTestLogger(this ILoggingBuilder builder)
+    public static ILoggingBuilder AddCache(this ILoggingBuilder builder)
     {
         builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, CacheLoggerProvider>());
         return builder;
     }
 
-    public static IReadOnlyCollection<LogEntry> GetTestLogs(this IServiceProvider services)
+    /// <summary>
+    /// Get the logs from <see cref="CacheLoggerProvider"/>.
+    /// </summary>
+    /// <param name="services">The service provider.</param>
+    /// <returns>The logs.</returns>
+    /// <exception cref="Exception">Thrown if there is no <see cref="CacheLoggerProvider"/>.</exception>
+    public static IReadOnlyCollection<LogEntry> GetCachedLogs(this IServiceProvider services)
     {
         CacheLoggerProvider provider = services
                                           .GetServices<ILoggerProvider>()
