bd1eda5cf3556fab93cd8c750c87bb39e724e56f
Laurence Elsdon <git@elsdon.me>
2024-02-29 09:54:56 +0000

    refactor(StudioLE.Conversion): Move non-essential methods to ConverterResolverHelpers

--- StudioLE.Conversion/src/ConverterResolver.cs
+++ StudioLE.Conversion/src/ConverterResolver.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-
 namespace StudioLE.Conversion;
 
 /// <summary>
@@ -63,78 +61,6 @@ public class ConverterResolver
                 .FirstOrDefault())
             .FirstOrDefault();
     }
-
-    /// <summary>
-    /// Resolve an converter instance that converts <paramref name="sourceType"/> to <paramref name="resultType"/>.
-    /// </summary>
-    /// <remarks>
-    /// This method will searched the full inheritance hierarchy of <paramref name="sourceType"/> and <paramref name="resultType"/> to find a converter.
-    /// The converter will be instantiated using <see cref="Activator.CreateInstance(Type)"/>.
-    /// </remarks>
-    /// <param name="sourceType">The <see cref="Type"/> to convert from.</param>
-    /// <param name="resultType">The <see cref="Type"/> to convert to.</param>
-    /// <returns>The converter <see cref="Type"/>, or null if no converter was found.</returns>
-    public object? ResolveActivated(Type sourceType, Type resultType)
-    {
-        Type? resolved = ResolveType(sourceType, resultType);
-        return resolved is null
-            ? null
-            : Activate(resultType, resolved);
-    }
-
-    /// <summary>
-    /// Try and convert <paramref name="source"/> to <paramref name="resultType"/> using the most appropriate converter.
-    /// </summary>
-    /// <param name="source">The instance to be converted.</param>
-    /// <param name="resultType">The <see cref="Type"/> to convert to.</param>
-    /// <returns><paramref name="source"/> converted to <paramref name="resultType"/>, or null if no converter was found.</returns>
-    public object? TryConvert(object source, Type resultType)
-    {
-        object? converter = ResolveActivated(source.GetType(), resultType);
-        if (converter is null)
-            return null;
-        MethodInfo? method = converter.GetType().GetMethod("Convert");
-        if (method is null)
-            return null;
-        return method.Invoke(converter, new[] { source });
-    }
-
-    /// <summary>
-    /// Try and convert <paramref name="source"/> to <typeparamref name="TResult"/> using the most appropriate converter.
-    /// </summary>
-    /// <param name="source">The instance to be converted.</param>
-    /// <typeparam name="TSource">The <see cref="Type"/> of <paramref name="source"/>.</typeparam>
-    /// <typeparam name="TResult">The <see cref="Type"/> to convert to.</typeparam>
-    /// <returns><paramref name="source"/> converted to <typeparamref name="TResult"/>, or null if no converter was found.</returns>
-    public TResult? TryConvert<TSource, TResult>(TSource source) where TResult : struct
-    {
-        object? converter = ResolveActivated(typeof(TSource), typeof(TResult));
-        if (converter is null)
-            return null;
-        MethodInfo? method = converter.GetType().GetMethod("Convert");
-        if (method is null)
-            return null;
-        object? result = method.Invoke(converter, new object[] { source! });
-        return result is TResult tResult
-            ? tResult
-            : null;
-    }
-
-    private static object? Activate(Type resultType, Type converterType)
-    {
-        ConstructorInfo[] constructors = converterType.GetConstructors();
-        ConstructorInfo? parameterlessConstructor = constructors
-            .FirstOrDefault(x => x.GetParameters().Length == 0);
-        if (parameterlessConstructor is not null)
-            return Activator.CreateInstance(converterType);
-        ConstructorInfo? typeConstructor = constructors
-            .FirstOrDefault(x => x.GetParameters().Length == 1
-            && x.GetParameters().First().ParameterType == typeof(Type));
-        if (typeConstructor is not null)
-            return Activator.CreateInstance(converterType, resultType);
-        return null;
-    }
-
 
     private static IReadOnlyCollection<Type> GetTypeHierarchy(Type type)
     {
