13a986bdf289c0f9b5b92d9b2c027537de1e0e5f
Laurence Elsdon <git@elsdon.me>
2024-04-06 12:26:34 +0100

    feat(StudioLE.Extensions.Logging.Console): Added logToStandardError parameter to AddBasicConsole extensions.

--- StudioLE.Extensions.Logging.Console/src/BasicConsoleFormatterExtensions.cs
+++ StudioLE.Extensions.Logging.Console/src/BasicConsoleFormatterExtensions.cs
@@ -11,11 +11,17 @@ public static class BasicConsoleFormatterExtensions
     /// Add a <see cref="Microsoft.Extensions.Logging.Console.ConsoleLogger"/> with a <see cref="BasicConsoleFormatter"/>.
     /// </summary>
     /// <param name="builder">The builder.</param>
+    /// <param name="logToStandardError">Should logs be output to standard error?</param>
     /// <returns>The builder.</returns>
-    public static ILoggingBuilder AddBasicConsole(this ILoggingBuilder builder)
+    public static ILoggingBuilder AddBasicConsole(this ILoggingBuilder builder, bool logToStandardError = false)
     {
         return builder
-            .AddConsole(options => options.FormatterName = BasicConsoleFormatter.FormatterName)
+            .AddConsole(options =>
+            {
+                options.FormatterName = BasicConsoleFormatter.FormatterName;
+                if (logToStandardError)
+                    options.LogToStandardErrorThreshold = LogLevel.Trace;
+            })
             .AddConsoleFormatter<BasicConsoleFormatter, BasicConsoleFormatterOptions>();
     }
 
@@ -23,12 +29,19 @@ public static class BasicConsoleFormatterExtensions
     /// Add a <see cref="Microsoft.Extensions.Logging.Console.ConsoleLogger"/> with a <see cref="BasicConsoleFormatter"/>.
     /// </summary>
     /// <param name="builder">The builder.</param>
+    /// <param name="logToStandardError">Should logs be output to standard error?</param>
     /// <param name="configure">A delegate to configure the <see cref="BasicConsoleFormatterOptions"/>.</param>
     /// <returns>The builder.</returns>
-    public static ILoggingBuilder AddBasicConsole(this ILoggingBuilder builder, Action<BasicConsoleFormatterOptions> configure)
+    public static ILoggingBuilder AddBasicConsole(this ILoggingBuilder builder, Action<BasicConsoleFormatterOptions> configure, bool logToStandardError = false)
     {
         return builder
-            .AddConsole(options => options.FormatterName = BasicConsoleFormatter.FormatterName)
+            .AddConsole(options =>
+            {
+                options.FormatterName = BasicConsoleFormatter.FormatterName;
+                if (logToStandardError)
+                    options.LogToStandardErrorThreshold = LogLevel.Trace;
+
+            })
             .AddConsoleFormatter<BasicConsoleFormatter, BasicConsoleFormatterOptions>(configure);
     }
 }
