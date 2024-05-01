6ca747fc26f81971ff27f86148d95dbf5c2cc632
Laurence Elsdon <git@elsdon.me>
2024-05-01 14:47:14 +0100

    feat(StudioLE.Storage): Improved logging of PhysicalFileReader

--- StudioLE.Storage/src/Files/PhysicalFileReader.cs
+++ StudioLE.Storage/src/Files/PhysicalFileReader.cs
@@ -31,22 +31,22 @@ public class PhysicalFileReader : IFileReader
     private Stream? ReadSync(string path)
     {
         if (!Directory.Exists(_options.RootDirectory))
-        {
-            _logger.Log(_options.LogLevel, "Failed to read file. The root directory does not exist.");
-            return null;
-        }
+            return Failed(path, "The root directory does not exist");
         string absolutePath = Path.Combine(_options.RootDirectory, path);
         absolutePath = Path.GetFullPath(absolutePath);
         if (!absolutePath.StartsWith(_options.RootDirectory))
-        {
-            _logger.Log(_options.LogLevel, "Failed to read file. The path is outside the root directory.");
-            return null;
-        }
+            return Failed(path, "The path is outside the root directory");
         if (!File.Exists(absolutePath))
-        {
-            _logger.Log(_options.LogLevel, "Failed to read file. The file does not exist.");
-            return null;
-        }
+            return Failed(path, "The file does not exist");
         return File.OpenRead(absolutePath);
+    }
+
+    private Stream? Failed(string path, string message)
+    {
+        if(string.IsNullOrEmpty(message))
+            _logger.Log(_options.LogLevel, "Failed to read file {Path}", path);
+        else
+            _logger.Log(_options.LogLevel, "Failed to read file {Path}. {Message}", path, message);
+        return null;
     }
 }
